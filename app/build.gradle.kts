d6fbe365af330b7aefc450930832e85e377e12a6
Spencer Stream <sprewstream@gmail.com>
2023-08-08 21:18:16 -0500

    Creating custom buttons for the keyboard so the background colors can be set when guesses are made

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
--- app/src/main/java/com/stream_suite/wordgame/ui/Keyboard.kt
+++ app/src/main/java/com/stream_suite/wordgame/ui/Keyboard.kt
@@ -1,22 +1,32 @@
 package com.stream_suite.wordgame.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.stream_suite.wordgame.R
+import com.stream_suite.wordgame.ui.theme.OffWhite
 
 @Composable
 fun Keyboard(onClickLetter: (Char) -> Unit, onClickDelete: () -> Unit, onClickSubmit: () -> Unit) {
@@ -32,9 +42,7 @@ fun Keyboard(onClickLetter: (Char) -> Unit, onClickDelete: () -> Unit, onClickSu
             }
         }
         KeyRow {
-            Button(
-                modifier = Modifier
-                    .wrapContentWidth(),
+            Button(modifier = Modifier.wrapContentWidth(),
                 contentPadding = PaddingValues(4.dp),
                 onClick = { onClickDelete() }) {
                 Icon(
@@ -45,9 +53,7 @@ fun Keyboard(onClickLetter: (Char) -> Unit, onClickDelete: () -> Unit, onClickSu
             "ZXCVBNM".forEach { letter ->
                 KeyButton(letter = letter, onClick = onClickLetter)
             }
-            Button(
-                modifier = Modifier
-                    .wrapContentWidth(),
+            Button(modifier = Modifier.wrapContentWidth(),
                 contentPadding = PaddingValues(4.dp),
                 onClick = { onClickSubmit() }) {
                 Icon(painter = painterResource(R.drawable.send_24), contentDescription = "Submit")
@@ -61,6 +67,7 @@ fun KeyRow(content: @Composable () -> Unit) {
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
         content()
     }
@@ -68,15 +75,53 @@ fun KeyRow(content: @Composable () -> Unit) {
 
 @Composable
 fun KeyButton(letter: Char, onClick: (Char) -> Unit) {
-    Button(
-        modifier = Modifier
-            .wrapContentWidth()
-            .width(32.dp),
-        contentPadding = PaddingValues(4.dp),
-        onClick = { onClick(letter) }) {
-        Text(
-            text = letter.toString()
-        )
+    ConstraintLayout(modifier = Modifier
+        .width(32.dp)
+        .wrapContentHeight()
+        .padding(vertical = 4.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .clickable { onClick(letter) }) {
+        val (key, boxOne, boxTwo, boxThree, boxFour) = createRefs()
+        val verticalMidpoint = createGuidelineFromStart(0.5f)
+        val horizontalMidpoint = createGuidelineFromTop(0.5f)
+
+        Box(modifier = Modifier
+            .background(OffWhite)
+            .constrainAs(boxOne) {
+                linkTo(top = parent.top, bottom = horizontalMidpoint)
+                linkTo(start = parent.start, end = verticalMidpoint)
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+            })
+        Box(modifier = Modifier
+            .background(OffWhite)
+            .constrainAs(boxTwo) {
+                linkTo(top = parent.top, bottom = horizontalMidpoint)
+                linkTo(start = verticalMidpoint, end = parent.end)
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+            })
+        Box(modifier = Modifier
+            .background(OffWhite)
+            .constrainAs(boxThree) {
+                linkTo(top = horizontalMidpoint, bottom = parent.bottom)
+                linkTo(start = parent.start, end = verticalMidpoint)
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+            })
+        Box(modifier = Modifier
+            .background(OffWhite)
+            .constrainAs(boxFour) {
+                linkTo(top = horizontalMidpoint, bottom = parent.bottom)
+                linkTo(start = verticalMidpoint, end = parent.end)
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+            })
+        Text(text = letter.toString(), modifier = Modifier
+            .constrainAs(key) {
+                centerTo(parent)
+            }
+            .padding(4.dp, 12.dp))
     }
 }
 
